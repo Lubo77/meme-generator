397659e70b7473570304751ba36a070dd1624960
Spyrius <61468175+Lubo77@users.noreply.github.com>
2022-12-21 11:19:17 +0100

    addition of inputs displayed on the image

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -21,6 +21,18 @@ export default function MainSection() {
             }
         })
     }
+
+    console.log(meme)
+
+    function handleChange(event) {
+        const {value, name} = event.target
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
     
     return (
         <main>
@@ -29,11 +41,17 @@ export default function MainSection() {
                     type="text"
                     placeholder="Top text"
                     className="input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="get-button"
@@ -42,7 +60,11 @@ export default function MainSection() {
                     GET A NEW IMAGE
                 </button>
 
-                <img src={meme.randomImage} className='meme--image' />
+                <div className="meme">
+                    <img src={meme.randomImage} className='meme--image' />
+                    <h2 className="meme--text top">{meme.topText}</h2>
+                    <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                </div>
             </div>
             
         </main>
